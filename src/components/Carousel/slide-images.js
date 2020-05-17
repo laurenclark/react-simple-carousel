9ef3f3522dc9229ebad135472dc970385150b6b3
Lauren <lauren.c4231@gmail.com>
2020-05-17 11:47:41 +0100

    Add images and image data

---
+++ src/components/Carousel/slide-images.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, name: 'coffee.jpg', alt: 'A cup of coffee with saucer and spoon' },
+    {
+        id: 2,
+        name: 'posing.jpg',
+        alt:
+            'A person posing in ski gear with a background of mountains and snow'
+    },
+    { id: 3, name: 'coins.jpg', alt: 'A stack of coins' },
+    { id: 4, name: 'raspberries.jpg', alt: 'A bowl of raspberries' }
+];
