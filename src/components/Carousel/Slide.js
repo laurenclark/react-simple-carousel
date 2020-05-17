e26a4f04fe32a0e667e51bf7a939a6c7612aebdc
Lauren <lauren.c4231@gmail.com>
2020-05-17 21:50:01 +0100

    Add view counter experiment

--- src/components/Carousel/Slide.js
+++ src/components/Carousel/Slide.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import slideImages from './slide-images';
 
-export default function Slide({ sourcePath, active, altText }) {
+export default function Slide({ sourcePath, isActive, altText, onPress }) {
     const baseURL = `/assets/images/`;
     const [viewed, setViewed] = useState(0);
 
     useEffect(() => {
-        if (active) {
+        if (isActive && sourcePath === sourcePath) {
             setViewed(viewed + 1);
         }
         return () => {};
     }, []);
+
     return (
-        <div className="slide">
-            <div className="slide__view-counter">Viewed {viewed}</div>
-            <img src={`${baseURL}${sourcePath}`} alt={altText} />
+        <div className="slide" onClick={onPress}>
+            <picture className="slide__inner">
+                <div className="slide__inner--mod">
+                    <div className="slide__view-counter">Viewed {viewed}</div>
+                    <img src={`${baseURL}${sourcePath}`} alt={altText} />
+                </div>
+            </picture>
         </div>
     );
 }
