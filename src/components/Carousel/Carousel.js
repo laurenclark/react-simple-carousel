96cd72efc13180afbe7de1df81180aa5f958fb3d
Lauren <lauren.c4231@gmail.com>
2020-05-20 03:50:28 +0100

    Remove Animations as they break the counter.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,7 +6,7 @@ import Arrow from './Arrow';
 
 function Carousel() {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [animation, setAnimation] = useState('');
+    const [animation, setAnimation] = useState();
     const imagesLength = imageData.length - 1;
     const handlers = useSwipeable({
         onSwipedLeft: () => nextHandler(),
@@ -16,31 +16,19 @@ function Carousel() {
     });
 
     const prevHandler = () => {
-        setAnimation('animate__slideInRight');
         const index = currentIndex === 0 ? imagesLength : currentIndex - 1;
         setCurrentIndex(index);
-        setTimeout(() => {
-            setAnimation('');
-        }, 200);
     };
 
     const nextHandler = () => {
-        setAnimation('animate__slideInLeft');
         const index = currentIndex === imagesLength ? 0 : currentIndex + 1;
         setCurrentIndex(index);
-        setTimeout(() => {
-            setAnimation('');
-        }, 200);
     };
 
     return (
         <div className="carousel" {...handlers}>
             <div className="carousel__track">
-                <Slide
-                    imageData={imageData}
-                    currentIndex={currentIndex}
-                    animation={animation}
-                />
+                <Slide imageData={imageData} currentIndex={currentIndex} />
             </div>
             <div className="controls">
                 <Arrow left="true" handleClick={prevHandler} />
--- src/components/Carousel/Slide.js
+++ src/components/Carousel/Slide.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Animate from 'animate.css';
 export default function Slide({ imageData, currentIndex, animation }) {
     const baseURL = `/assets/images/`;
     const image = imageData[currentIndex];
 
     return (
-        <div className={`slide animate__animated animate__fast ${animation}`}>
+        <div className="slide">
             <div className="slide__counter">
                 Viewed {(image.count += 1)} times
             </div>
